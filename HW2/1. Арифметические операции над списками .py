7cc9dc026ca27df53cc3cd141dce26b18c0b245d
DNayd <93384678+DNayd@users.noreply.github.com>
2021-12-21 01:27:04 +0300

    1. Арифметические операции над списками .py

--- HW2/1. Арифметические операции над списками .py
+++ HW2/1. Арифметические операции над списками .py
@@ -21,6 +21,10 @@ def log(l):
   g = np.log(l)
   return g
 
+print('Калькулятор для сложения (1), вычитания (2), умножения (3),'
+      'деления (4), возведения в степень (5), извлечения корня (6) и логарифмирования (7)')
+action = input('Выберите действие: ')
+
 x = int(input('Сколько списков необходимо пересчитать? '))
 lists = []
 
@@ -35,11 +39,20 @@ for i in range(len(lists)):
 print(lists)
 lists = np.array(lists)
 
-print(calc(lists, add))
-print(calc(lists, sub))
-print(calc(lists, mul))
-print(calc(lists, truediv))
-print(power(lists, int(input('Степень: '))))
-print(sqrt(lists, int(input('Степень корня: '))))
-print(log(lists))
+if action == '1':
+  print(calc(lists, add))
+elif action == '2':
+  print(calc(lists, sub))
+elif action == '3':
+  print(calc(lists, mul))
+elif action == '4':
+  print(calc(lists, truediv))
+elif action == '5':
+  print(power(lists, int(input('Степень: '))))
+elif action == '6':
+  print(sqrt(lists, int(input('Степень корня: '))))
+elif action == '7':
+  print(log(lists))
+else:
+  print('Ошибка ввода')
 
