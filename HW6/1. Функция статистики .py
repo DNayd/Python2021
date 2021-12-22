977f22caf7c5c5c8fbb8d9440acf8030b2604aad
DNayd <93384678+DNayd@users.noreply.github.com>
2021-12-22 18:30:16 +0300

    1. Функция статистики .py

--- HW6/1. Функция статистики .py
+++ HW6/1. Функция статистики .py
@@ -1,3 +1,4 @@
+# В идеале:
 def Stat(GG):
   A = alphabet
   for n in GG:
@@ -8,3 +9,11 @@ def Stat(GG):
       A[n] = 1
   return 
 print(Stat('sequence'))
+
+# Вероятнее всего так будет работать, но не факт :)
+def alph():
+  A = input('Введите алфавит: ').split()
+  seq = input('Введите последовательность: ')
+  for i in range(len(seq)):
+      G = seq.count(A[i])
+      print(A[i], G)
